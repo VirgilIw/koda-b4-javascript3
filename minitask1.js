8beb35244c711330daf1e85440acdbc4d92579f6
VirgilIw <imanuelambar11@gmail.com>
2025-09-08 09:58:14 +0700

    Add files via upload

--- minitask1.js
+++ minitask1.js
@@ -1,11 +1,16 @@
 // mencari 1 build-in method atau function
 // membuat proses manual sesuai  dengan built in method atau function yang ditemukan
 
-const userName = ["gil", "piu", "iw", "amb"];
+let userName = ["gil", "piu", "iw", "amb"];
 
 for (i = 0; i < userName.length; i++) {
   console.log(userName[i]);
 }
+
+// manual
+userName.length = 3;
+
+// method
 const removeLast = userName.pop();
 
 console.log(removeLast);
